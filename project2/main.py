c3a5b632501176388ef6c5a44a87741beee83a2b
soonerfan237 <soonerfan237@gmail.com>
2019-04-22 12:36:04 -0500

    Started implementing feature which is length of review text.

--- project2/main.py
+++ project2/main.py
@@ -14,7 +14,7 @@ from nltk import sent_tokenize
 from nltk import word_tokenize
 from nltk import pos_tag
 from nltk import ne_chunk
-
+from nltk.classify import apply_features
 
 def get_entity(text):
     """Prints the entity inside of the text."""
@@ -22,6 +22,8 @@ def get_entity(text):
         for chunk in ne_chunk(pos_tag(word_tokenize(sent))):
             if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
                 print(chunk.label(), ' '.join(c[0] for c in chunk.leaves()))
+                feature_textlength = get_feature_textlength(text)
+                print(feature_textlength)
 
 
 def doextraction(glob_text):
@@ -29,8 +31,11 @@ def doextraction(glob_text):
     for thefile in glob.glob(glob_text):
         with io.open(thefile, 'r', encoding='utf-8') as fyl:
             text = fyl.read()
+            print(text)
             get_entity(text)
 
+def get_feature_textlength(text):
+    return {'textlength' : len(text)}
 
 if __name__ == '__main__':
     # Usage: python3 entity-extractor.py 'train/pos/*.txt'

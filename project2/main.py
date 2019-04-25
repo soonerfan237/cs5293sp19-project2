ce8ce907f8e7bbc48bcae215b670cb63f3eb665a
soonerfan237 <soonerfan237@gmail.com>
2019-04-25 12:48:00 -0500

    Started adding naive bayes classifier.

--- project2/main.py
+++ project2/main.py
@@ -16,12 +16,17 @@ from nltk import pos_tag
 from nltk import ne_chunk
 from nltk.classify import apply_features
 
+labels_text_name = []
+
 def get_entity(text):
     """Prints the entity inside of the text."""
     for sent in sent_tokenize(text):
         for chunk in ne_chunk(pos_tag(word_tokenize(sent))):
             if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
-                print(chunk.label(), ' '.join(c[0] for c in chunk.leaves()))
+                #print(chunk.label(), ' '.join(c[0] for c in chunk.leaves()))
+                name = ' '.join(c[0] for c in chunk.leaves())
+                print(name)
+                label_text(text,name)
                 feature_textlength = get_feature_textlength(text)
                 print(feature_textlength)
 
@@ -33,6 +38,19 @@ def doextraction(glob_text):
             text = fyl.read()
             print(text)
             get_entity(text)
+
+    for label in labels_text_name:
+        print(label)
+
+    featuresets = [(get_feature_textlength(n), name) for (n, name) in labels_text_name]
+    classifier = nltk.NaiveBayesClassifier.train(featuresets)
+    result = classifier.classify(get_feature_textlength(text))
+    print("RESULTS===================")
+    print(result)
+
+def label_text(text,name):
+    labels_text_name.append([text,name])
+    
 
 def get_feature_textlength(text):
     return {'textlength' : len(text)}

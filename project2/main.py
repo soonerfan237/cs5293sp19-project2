23cffa654143d7486a591e03baff444a250aa40f
soonerfan237 <soonerfan237@gmail.com>
2019-04-26 23:11:48 -0500

    Created redacted test files.  Added more features.

--- project2/main.py
+++ project2/main.py
@@ -7,6 +7,7 @@ import os
 import pdb
 import sys
 
+import re
 
 sys.path.append("/usr/lib/python3/dist-packages")
 import nltk
@@ -14,15 +15,17 @@ from nltk import sent_tokenize
 from nltk import word_tokenize
 from nltk import pos_tag
 from nltk import ne_chunk
+from nltk.corpus import stopwords
 from nltk.classify import apply_features
 
 labels_text_name = []
+entity_set_test = []
 
-def get_entity(text_path):
+def get_entity_training(text_path):
     """Prints the entity inside of the text."""
     with io.open(text_path, 'r', encoding='utf-8') as fyl:
         text = fyl.read()
-        print(text)
+        #print(text)
         #get_entity(text)
         for sent in sent_tokenize(text):
             for chunk in ne_chunk(pos_tag(word_tokenize(sent))):
@@ -31,42 +34,105 @@ def get_entity(text_path):
                     name = ' '.join(c[0] for c in chunk.leaves())
                     print(name)
                     labels_text_name.append([text_path,name])
-                    #features = get_features(text)
-                    #feature_textlength = get_feature_textlength(text)
+                    #features = get_features(text_path,name)
                     #print(features)
+
+def get_entity_test(text_path):
+    entity = ""
+    with io.open(text_path, 'r', encoding='utf-8') as fyl:
+        text = fyl.read()
+        print (text)
+        matches = re.findall(r'XX* ?X*? ?X*?',text)
+        print (matches)
+        for match in matches:
+            match = match.strip()
+            entity_set_test.append([text_path,match])
 
 def doextraction(glob_text):
     """Get all the files from the given glob and pass them to the extractor."""
+    count_training_files = 0
     for text_path in glob.glob(glob_text):
+        count_training_files = count_training_files + 1
         print("text_path = " + text_path)
-        get_entity(text_path)
-        #with io.open(text_path, 'r', encoding='utf-8') as fyl:
-            #text = fyl.read()
-            #print(text)
-            #get_entity(text)
-
+        if not ("urls_" in text_path):
+            print("valid path....")
+            get_entity_training(text_path)
+        if count_training_files > 100:
+            print("checked enough training files... moving on.")
+            break
+    print ("about to start printing labels")
     for label in labels_text_name:
         print(label)
     
-    featuresets = []
+    print ("about to start getting featuresets")
+    featuresets_training = []
     for (text_path, name) in labels_text_name:
-        featuresets.append([get_features(text_path),name])
-    #featuresets = [(get_features(n), name) for (n, name) in labels_text_name]
-    classifier = nltk.NaiveBayesClassifier.train(featuresets)
-    result = classifier.classify(get_features(text_path))
-    print("RESULTS===================")
-    print(result)
-
-#def label_text(text_path,name):
-    #labels_text_name.append([text_path,name])
+        featuresets_training.append([get_features_training(text_path,name),name])
+    classifier = nltk.NaiveBayesClassifier.train(featuresets_training)
     
-def get_features(text_path):
+    count_test_files = 0
+    for text_path in glob.glob('aclImdb/test_redacted/*/*.txt.redacted'):
+        count_test_files = count_test_files + 1
+        print("text_path = " + text_path)
+        if not ("urls_" in text_path):
+            print("valid path....")
+            get_entity_test(text_path)
+        if count_test_files > 100:
+            print("checked enough test files... moving on.")
+            break
+    for entity in entity_set_test:
+        print(entity)
+        result = classifier.classify(get_features_test(entity[0], entity[1]))
+        print("RESULTS===================")
+        print(result)
+    
+def get_features_training(text_path,name):
     with io.open(text_path, 'r', encoding='utf-8') as fyl:
         text = fyl.read()
-    return {'textlength' : len(text)}
+    sentiment = 0 #sentiment 1 corresponds to unsup folder
+    if "pos" in text_path:
+        sentiment = 2
+    elif "neg" in text_path:
+        sentiment = 1
+    most_common_word = ""
+    word_list = text.split()
+    #word_list = [word for word in word_list if word not in stopwords.words('english')]
+    word_dict = dict((word,1) for word in word_list)
+    for word in word_list:
+        word_dict[word] = word_dict[word] + 1
+    v=list(word_dict.values())
+    k=list(word_dict.keys())
+    most_common_word = k[v.index(max(v))]
+    num_of_spaces = 0
+    for character in name:
+        if character == " ":
+            num_of_spaces = num_of_spaces + 1
+    return {'textlength' : int(len(text)/10), 'sentiment' : sentiment, 'namelength' : len(name),'most_common_word' : most_common_word, 'num_of_spaces' : num_of_spaces}
 
-#def get_feature_textlength(text):
-#    return {'textlength' : len(text)}
+def get_features_test(text_path,name):
+    with io.open(text_path, 'r', encoding='utf-8') as fyl:
+        text = fyl.read()
+    sentiment = 0 #sentiment 1 corresponds to unsup folder
+    if "pos" in text_path:
+        sentiment = 2
+    elif "neg" in text_path:
+        sentiment = 1
+    most_common_word = ""
+    word_list = text.split()
+    #word_list = [word for word in word_list if word not in stopwords.words('english')]
+    word_dict = dict((word,1) for word in word_list)
+    for word in word_list:
+        word_dict[word] = word_dict[word] + 1
+    v=list(word_dict.values())
+    k=list(word_dict.keys())
+    most_common_word = k[v.index(max(v))]
+    num_of_spaces = 0
+    for character in name:
+        if character == " ":
+            num_of_spaces = num_of_spaces + 1
+    return {'textlength' : int(len(text)/10), 'sentiment' : sentiment, 'namelength' : len(name),'most_common_word' : most_common_word, 'num_of_spaces' : num_of_spaces}
+
+
 
 if __name__ == '__main__':
     # Usage: python3 entity-extractor.py 'train/pos/*.txt'
